fa779bc021f1c9922ff9d26bad1b5d62cb7c51f9
numen-fan <1414340590@qq.com>
2021-09-13 23:37:52 +0800

    项目搭建

---
+++ app/src/main/java/com/jiajia/random/RandomApplication.kt
@@ -0,0 +1,4 @@
+package com.jiajia.random
+
+class RandomApplication {
+}
---
+++ app/src/main/java/com/jiajia/random/sunnyweather/ui/SunnyWeatherMainActivity.kt
@@ -0,0 +1,9 @@
+package com.jiajia.random.sunnyweather.ui
+
+/**
+ * Created by fanjiajia on 2021-09-10
+ * 天气模块主页面
+ */
+class SunnyWeatherMainActivity {
+
+}
